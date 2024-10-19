31cc05bc2898872e345998e97d106c5c12e45c20
vova <your.email@example.com>
2024-10-19 20:28:45 +0300

    config

--- technogroup-app/next.config.mjs
+++ technogroup-app/next.config.mjs
@@ -1,7 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
+  images: {
+    domains: ['localhost', '127.0.0.1'],
+    unoptimized: true,
+  },
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
+  output: 'static',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
